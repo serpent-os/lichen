1d6f44c068bcbbe9decf8986f9778edd6f97bfa7
Ikey Doherty <ikey@serpentos.com>
2024-06-02 20:10:41 +0100

    pages: Add dummy welcome page (WIP)
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ src/pages/welcome.rs
@@ -0,0 +1,53 @@
+// SPDX-FileCopyrightText: Copyright © 2024 Serpent OS Developers
+//
+// SPDX-License-Identifier: MPL-2.0
+
+//! Welcome page
+
+use ratatui::{
+    style::{Style, Stylize},
+    text::{Line},
+    widgets::{self, Wrap},
+};
+
+use crate::Component;
+
+pub struct Welcome {}
+
+impl Default for Welcome {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Welcome {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl Component for Welcome {
+    fn render(&self, frame: &mut ratatui::prelude::Frame, area: ratatui::prelude::Rect) {
+        let intro = widgets::Paragraph::new(vec![
+            Line::from("Are you ready to install Serpent OS?").style(Style::default().bold()).alignment(ratatui::layout::Alignment::Center),
+            Line::from(""), Line::from(""),
+            Line::from("📋 Disclaimer").style(Style::default().bold()),
+            Line::from(""),
+            Line::from("Serpent OS is not ready for production use nor should it be used for market predictions, nuclear facility management or the care of infants. At any point your installation may encounter errors or in fact no longer have ever been installed due to temporal fluctations resulting from the misuse of this install in a particle accelerator.").style(Style::default().dim())
+        ]).wrap(Wrap{ trim: false });
+
+        frame.render_widget(intro, area)
+    }
+
+    fn update(&mut self, _: crate::Action) -> Option<crate::Action> {
+        None
+    }
+
+    fn state(&self) -> crate::State {
+        crate::State::NONE
+    }
+
+    fn push_state(&mut self, _: crate::State) {}
+
+    fn pop_state(&mut self, _: crate::State) {}
+}
