19ad731a120b8ec569556e68b35b7f8300b20512
Ikey Doherty <ikey@serpentos.com>
2024-06-19 11:16:58 +0100

    lichen_cli: Add basic partition enumeration
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- lichen_cli/src/main.rs
+++ lichen_cli/src/main.rs
@@ -4,19 +4,50 @@
 
 //! Super basic CLI runner for lichen
 
+use std::fmt::Display;
+
 use console::{set_colors_enabled, style};
 use dialoguer::theme::ColorfulTheme;
 use system::{
-    disk::Disk,
+    disk::{Disk, Partition},
     locale::{self, Locale, Registry},
 };
 use tokio::process::Command;
 
+/// Installation partitions
+#[derive(Debug)]
+enum InstallationPartiton {
+    /// Boot with optional xbootldr pair
+    Boot {
+        esp: Partition,
+        xbootldr: Option<Partition>,
+    },
+    /// Standard partition target
+    Normal(Partition),
+}
+
+impl Display for InstallationPartiton {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self {
+            InstallationPartiton::Boot { esp, xbootldr } => {
+                if let Some(xbootldr) = xbootldr.as_ref() {
+                    f.write_fmt(format_args!("ESP {} with XBOOTLDR: {}", esp, xbootldr))
+                } else {
+                    f.write_fmt(format_args!("ESP {}", esp))
+                }
+            }
+            InstallationPartiton::Normal(part) => f.write_fmt(format_args!("Regular partition: {}", part)),
+        }
+    }
+}
+
+/// Craptastic header printing
 fn print_header(icon: &str, text: &str) {
     println!("\n\n  {}   {}", style(icon).cyan(), style(text).bright().bold());
     println!("\n\n")
 }
 
+/// Crappy print of a summary field
 fn print_summary_item(name: &str, item: &impl ToString) {
     let name = console::pad_str(name, 10, console::Alignment::Left, None);
     println!("      {}   -  {}", style(name).bold(), item.to_string());
@@ -55,21 +86,38 @@ fn ask_password() -> color_eyre::Result<String> {
 }
 
 /// TODO: Make this actually use more than Disk and be async!
-fn load_disks() -> color_eyre::Result<Vec<Disk>> {
-    Ok(Disk::discover()?)
-}
+fn load_partitions() -> color_eyre::Result<Vec<InstallationPartiton>> {
+    let disks = Disk::discover()?;
+    let mut partitions = vec![];
 
-/// Ask the user where to install
-fn ask_disk(disks: &[Disk]) -> color_eyre::Result<&Disk> {
-    print_header("🖴", "We need to pick a location to install Serpent OS");
+    for disk in disks {
+        // Ignore errors.
+        if let Ok(parts) = disk.partitions() {
+            // Find first ESP
+            let esp = parts
+                .iter()
+                .find(|p| matches!(p.kind, system::disk::PartitionKind::ESP));
 
-    let index = dialoguer::Select::with_theme(&ColorfulTheme::default())
-        .with_prompt("Select a disk from the following list")
-        .items(disks)
-        .default(0)
-        .interact()?;
+            // Find first xbootldr
+            let xbootldr = parts
+                .iter()
+                .find(|p| matches!(p.kind, system::disk::PartitionKind::XBOOTLDR));
 
-    Ok(&disks[index])
+            if let Some(esp) = esp {
+                partitions.push(InstallationPartiton::Boot {
+                    esp: esp.clone(),
+                    xbootldr: xbootldr.cloned(),
+                });
+            }
+
+            // Allow "normal partitions" as installation targets
+            let regulars = parts
+                .iter()
+                .filter(|p| matches!(p.kind, system::disk::PartitionKind::Regular));
+            partitions.extend(regulars.map(|p| InstallationPartiton::Normal(p.clone())))
+        }
+    }
+    Ok(partitions)
 }
 
 fn ask_timezone() -> color_eyre::Result<String> {
@@ -92,18 +140,20 @@ async fn main() -> color_eyre::Result<()> {
     set_colors_enabled(true);
 
     // Load all the things
-    let disks = load_disks()?;
+    let partitions = load_partitions()?;
+    for part in partitions {
+        eprintln!("Partition: {}", part);
+    }
+
     // TODO: Make Registry use asynchronous loading
     let registry = locale::Registry::new()?;
     let locales = load_locales(&registry).await?;
 
-    let selected_disk = ask_disk(&disks)?;
     let selected_locale = ask_locale(&locales).await?;
     let timezone = ask_timezone()?;
     let _ = ask_password()?;
 
     print_header("🕮", "Quickly review your settings");
-    print_summary_item("Disk", selected_disk);
     print_summary_item("Locale", selected_locale);
     print_summary_item("Timezone", &timezone);
 
