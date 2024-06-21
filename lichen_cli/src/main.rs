0d11d3f48956b6a9925d11a3f6d1959806a73e01
Ikey Doherty <ikey@serpentos.com>
2024-06-21 21:54:15 +0100

    lichen_cli: Last in-place hacks for disk enumeration
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- lichen_cli/src/main.rs
+++ lichen_cli/src/main.rs
@@ -16,40 +16,40 @@ use system::{
 };
 use tokio::process::Command;
 
-/// Installation partitions
+/// Special case handling of boot partitions
+/// to join them to related XBOOTLDR
 #[derive(Debug)]
-enum InstallationPartiton {
-    /// Boot with optional xbootldr pair
-    Boot {
-        esp: Partition,
-        xbootldr: Option<Partition>,
-    },
-    /// Standard partition target
-    Normal(Partition),
+struct BootPartition {
+    esp: Partition,
+    xbootldr: Option<Partition>,
 }
 
-impl Display for InstallationPartiton {
+impl Display for BootPartition {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match &self {
-            InstallationPartiton::Boot { esp, xbootldr } => {
-                if let Some(xbootldr) = xbootldr.as_ref() {
-                    f.write_fmt(format_args!(
-                        "ESP on {} ({}) with XBOOTLDR: {} ({})",
-                        esp,
-                        HumanBytes(esp.size),
-                        xbootldr,
-                        HumanBytes(xbootldr.size)
-                    ))
-                } else {
-                    f.write_fmt(format_args!("ESP on {} ({})", esp, HumanBytes(esp.size)))
-                }
-            }
-            InstallationPartiton::Normal(part) => f.write_fmt(format_args!(
-                "Regular partition on {} ({})",
-                part,
-                HumanBytes(part.size)
-            )),
+        if let Some(xbootldr) = self.xbootldr.as_ref() {
+            f.write_fmt(format_args!(
+                "ESP on {} ({}) with XBOOTLDR: {} ({})",
+                self.esp,
+                HumanBytes(self.esp.size),
+                xbootldr,
+                HumanBytes(xbootldr.size)
+            ))
+        } else {
+            f.write_fmt(format_args!("ESP on {} ({})", self.esp, HumanBytes(self.esp.size)))
         }
+    }
+}
+
+#[derive(Debug)]
+struct TargetPartition(pub Partition);
+
+impl Display for TargetPartition {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!(
+            "Regular partition on {} ({})",
+            self.0,
+            HumanBytes(self.0.size)
+        ))
     }
 }
 
@@ -61,7 +61,7 @@ fn print_header(icon: &str, text: &str) {
 
 /// Crappy print of a summary field
 fn print_summary_item(name: &str, item: &impl ToString) {
-    let name = console::pad_str(name, 10, console::Alignment::Left, None);
+    let name = console::pad_str(name, 20, console::Alignment::Left, None);
     println!("      {}   -  {}", style(name).bold(), item.to_string());
 }
 
@@ -98,9 +98,10 @@ fn ask_password() -> color_eyre::Result<String> {
 }
 
 /// TODO: Make this actually use more than Disk and be async!
-fn load_partitions() -> color_eyre::Result<Vec<InstallationPartiton>> {
+fn load_partitions() -> color_eyre::Result<(Vec<BootPartition>, Vec<TargetPartition>)> {
     let disks = Disk::discover()?;
     let mut partitions = vec![];
+    let mut boots = vec![];
 
     for disk in disks {
         // Ignore errors.
@@ -116,17 +117,19 @@ fn load_partitions() -> color_eyre::Result<Vec<InstallationPartiton>> {
                 .find(|p| matches!(p.kind, system::disk::PartitionKind::XBOOTLDR));
 
             if let Some(esp) = esp {
-                partitions.push(InstallationPartiton::Boot {
+                boots.push(BootPartition {
                     esp: esp.clone(),
                     xbootldr: xbootldr.cloned(),
-                });
+                })
             }
 
             // Allow "normal partitions" as installation targets
             let regulars = parts
                 .iter()
-                .filter(|p| matches!(p.kind, system::disk::PartitionKind::Regular));
-            partitions.extend(regulars.map(|p| InstallationPartiton::Normal(p.clone())))
+                .filter(|p| matches!(p.kind, system::disk::PartitionKind::Regular))
+                .cloned()
+                .map(TargetPartition);
+            partitions.extend(regulars);
         }
     }
 
@@ -136,20 +139,13 @@ fn load_partitions() -> color_eyre::Result<Vec<InstallationPartiton>> {
     }
 
     // ensure we have an ESP..
-    let esp = partitions
-        .iter()
-        .find(|p| matches!(p, InstallationPartiton::Boot { esp: _, xbootldr: _ }));
-    if let Some(InstallationPartiton::Boot { esp, xbootldr }) = esp {
-        if xbootldr.is_none() {
-            eprintln!("Warning: No XBOOTLDR available for ESP: {}", esp.path.display());
-        }
-    } else {
-        return Err(eyre!("No usable EFI System Partition").suggestion(
+    if boots.is_empty() {
+        Err(eyre!("No usable EFI System Partition").suggestion(
             "The installer requires that an ESP is present, and will also use an XBOOTLDR partition if present",
-        ));
+        ))
+    } else {
+        Ok((boots, partitions))
     }
-
-    Ok(partitions)
 }
 
 fn ask_timezone() -> color_eyre::Result<String> {
@@ -166,16 +162,35 @@ fn ask_timezone() -> color_eyre::Result<String> {
     Ok(chrono_tz::TZ_VARIANTS[index].to_string())
 }
 
+/// Pick an ESP please...
+fn ask_esp(parts: &[BootPartition]) -> color_eyre::Result<&BootPartition> {
+    print_header("", "Please choose an EFI System Partition for Serpent OS to use");
+    let index = dialoguer::Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Pick a suitably sized ESP")
+        .items(parts)
+        .default(0)
+        .interact()?;
+    Ok(&parts[index])
+}
+
+/// Where's it going?
+fn ask_rootfs(parts: &[TargetPartition]) -> color_eyre::Result<&TargetPartition> {
+    print_header("", "Please choose a partition to format and install Serpent OS to (/)");
+    let index = dialoguer::Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Pick a suitably sized partition")
+        .items(parts)
+        .default(0)
+        .interact()?;
+    Ok(&parts[index])
+}
+
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
     color_eyre::install().unwrap();
     set_colors_enabled(true);
 
     // Load all the things
-    let partitions = load_partitions()?;
-    for part in partitions {
-        eprintln!("Partition: {}", part);
-    }
+    let (boots, parts) = load_partitions()?;
 
     // TODO: Make Registry use asynchronous loading
     let registry = locale::Registry::new()?;
@@ -185,9 +200,14 @@ async fn main() -> color_eyre::Result<()> {
     let timezone = ask_timezone()?;
     let _ = ask_password()?;
 
+    let esp = ask_esp(&boots)?;
+    let rootfs = ask_rootfs(&parts)?;
+
     print_header("🕮", "Quickly review your settings");
     print_summary_item("Locale", selected_locale);
     print_summary_item("Timezone", &timezone);
+    print_summary_item("Bootloader", esp);
+    print_summary_item("Root (/) filesystem", rootfs);
 
     println!("\n\n");
 
